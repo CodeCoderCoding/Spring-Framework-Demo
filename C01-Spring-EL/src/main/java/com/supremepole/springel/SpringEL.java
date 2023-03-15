aa0cdfaff0295f58a8b395baafd6244cf613b633
CodeCoderCoding <kneil5778@gmail.com>
2023-03-16 00:18:14 +0800

    update SpringEL

--- C01-Spring-EL/src/main/java/com/supremepole/springel/SpringEL.java
+++ C01-Spring-EL/src/main/java/com/supremepole/springel/SpringEL.java
@@ -15,13 +15,19 @@ public class SpringEL {
     private String osName;
 
     // 注入其他bean的属性
-    @Value("#{person.name}")
-    private String personName;
+//    @Value("#{person.name}")
+//    private String personName;
+
+    // 注入其他bean的属性，和上面的写法等价
+    @Value("#{person['name1']}")
+    private String personName1;
+
 
     public void output(){
         System.out.println(normalString);
         System.out.println(osName);
-        System.out.println(personName);
+//        System.out.println(personName);
+        System.out.println(personName1);
     }
 
 }
