4faa32235f89ceb6107795294d1908801a9d04f1
CodeCoderCoding <kneil5778@gmail.com>
2023-03-17 11:30:40 +0800

    update SpringEL

--- A04-Spring-FactoryBean/A04-Spring-FactoryBean/src/main/java/com/supremepole/springfactorybean/FactoryBeanTest.java
+++ A04-Spring-FactoryBean/A04-Spring-FactoryBean/src/main/java/com/supremepole/springfactorybean/FactoryBeanTest.java
@@ -7,9 +7,9 @@ public class FactoryBeanTest {
     public static void main(String[] args){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         Object object=  applicationContext.getBean("factoryBeanPojo");
-        FactoryBeanPojo factoryBeanPojo= (FactoryBeanPojo) applicationContext.getBean("&factoryBeanPojo");
+        Object Object1= (FactoryBeanPojo) applicationContext.getBean("&factoryBeanPojo");
         System.out.println(object.getClass().getName());
-        System.out.println(factoryBeanPojo.getClass().getName());
+        System.out.println(Object1.getClass().getName());
     }
 
 }
