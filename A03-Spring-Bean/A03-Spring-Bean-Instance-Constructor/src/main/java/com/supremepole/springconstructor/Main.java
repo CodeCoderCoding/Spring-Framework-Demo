f81c6b33fe5f7b21225a362e59409ea4bcdf1516
CodeCoderCoding <kneil5778@gmail.com>
2023-04-10 10:23:38 +0800

    add module spring-bean-instance-constructor

---
+++ A03-Spring-Bean/A03-Spring-Bean-Instance-Constructor/src/main/java/com/supremepole/springconstructor/Main.java
@@ -0,0 +1,13 @@
+package com.supremepole.springconstructor;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Website website=(Website) applicationContext.getBean("website");
+        System.out.println(website.getName());
+    }
+
+}
---
+++ A03-Spring-Bean/A03-Spring-Bean-Instance-Constructor/src/main/java/com/supremepole/springconstructor/Website.java
@@ -0,0 +1,28 @@
+package com.supremepole.springconstructor;
+
+public class Website {
+
+    private String name;
+    private String url;
+
+    public Website(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
